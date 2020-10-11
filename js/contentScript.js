6d1d7e1f6a080d871caf4f93db91db3f6d69f45a
Lenovo <Lenovo@windows.com>
2020-10-12 01:42:34 +0800

    修改計算規則

--- js/contentScript.js
+++ js/contentScript.js
@@ -4,11 +4,12 @@ var requst_source = "content";
 var dto = {code: "", msg: "", url: "", element: "", option: "", content: "", type: "", source: requst_source};
 var response_dto = {code: response_success, source: requst_source};
 var timer = null;
+var timer_stop = null;
 var cover_content = '<div style="position: fixed;top: 0px;opacity: 0.5;left: 0px;z-index: 1040;width: 100vw;height: 100vh;background-color: rgb(0, 0, 0);"></div>' +
     '<div style="position: fixed;z-index: 1041;background-color: white;padding: 16px;border-radius: 5px;font-size: 1rem;left: 50%;top: 40%;transform: translateX(-50%);font-weight: 300;">' +
     '<div style="margin-bottom: 10px">' +
     '<button type="button" class="btn btn-outline-primary" style="width: 200px;display: block;margin: 0 auto;" onclick="copy_cover()" >复制</button>' +
-    '<button style="position: absolute;right: 16px;top: 16px;" type="button" class="btn btn-outline-primary" onclick="document.getElementById(\'cover\').style.display =\'none\';clearInterval(timer);timer = null;">X</button>' +
+    '<button id="cover_close" style="position: absolute;right: 16px;top: 16px;" type="button" class="btn btn-outline-primary" onclick="document.getElementById(\'cover\').style.display =\'none\';">X</button>' +
     '</div>' +
     '<div style="text-align: center;height: auto;max-height: 400px;overflow-y: auto;">' +
     '<div style="display: inline-block;vertical-align: middle;"><img style="display: inline-block;vertical-align: middle;" width="50" src="https://timgsa.baidu.com/timg?image&amp;quality=80&amp;size=b9999_10000&amp;sec=1602329894089&amp;di=d74ebf0e63b11375bc6b5c687ac394de&amp;imgtype=0&amp;src=http%3A%2F%2Fimg.ui.cn%2Fdata%2Ffile%2F4%2F9%2F2%2F2108294.gif">处理中...</div>' +
@@ -44,7 +45,7 @@ var script = '<script type="text/javascript">\n' +
     '   } else {\n' +
     '       return false;\n' +
     '   }\n' +
-    '}' +
+    '}\n' +
     '</script>';
 $("body").append('<div id="cover" style="display: none">' + cover_content + '</div>');
 $("body").append(script);
@@ -59,6 +60,14 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     } else if (request.cmd == "copy_order_address") {
         if (request.request.code == response_success) {
             $('#cover').show(800);
+            timer_stop = setInterval((function () {
+                if($('#cover').css("display") == 'none'){
+                    clearInterval(timer);
+                    clearInterval(timer_stop);
+                    timer_stop = null;
+                    timer = null;
+                }
+            }),100);
             let i = 0;
             timer = setInterval((function () {
                 var tr_arr = $($("table")[1]).find("tr");
@@ -69,21 +78,24 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 } else {
                     do {
                         i++;
-                        if(i >= tr_arr.length){
+                        if (i >= tr_arr.length) {
                             break;
                         }
                         td = $(tr_arr[i]).find("td:nth-child(3)");
                         className = $(td).find("i").attr("class");
-                        if(className.indexOf("unlock") == -1){
+                        if (className.indexOf("unlock") == -1) {
                             $(td).find("i").click();
                             break;
                         }
                     } while (true)
                 }
                 var scroll = $(document.querySelector(".TB_outerWrapper_4-62-1.TB_bordered_4-62-1.TB_notTreeStriped_4-62-1").children[0].children[0].children[1].children[0]);
-                scroll.animate({scrollTop: 60 * (i+1)}, 1000);
+                if (i > 8) {
+                    scroll.animate({scrollTop: 60 * (i + 1)-60}, 1000);
+                }
                 var arr_index = [2, 11, 10, 3, 4, 5, 6];
                 if (i >= tr_arr.length) {
+
                     //$('#cover').hide(800);
                     clearInterval(timer);
                     var new_table = $("<table></table>");
@@ -100,7 +112,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                             text_td = text_td.replace("回收单号", "");
                             text_td = text_td.replace("锁定", "");
                             new_td.text(text_td);
-                            if(text_td.split("/").length == 3){
+                            if (text_td.split("/").length == 3) {
                                 var add = text_td.split("/");
                                 var new_td_0 = $("<td></td>");
                                 new_td_0.css("border", "1px solid #d4d5d5");
@@ -117,13 +129,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                                 new_tr.append(new_td_0);
                                 new_tr.append(new_td_1);
                                 new_tr.append(new_td_2);
-                            }else{
+                            } else {
                                 new_tr.append(new_td);
                             }
                         }
                         new_table.append(new_tr);
                     }
-                    $(document.getElementById("cover").children[1]).css("top","20%");
+                    $(document.getElementById("cover").children[1]).css("top", "20%");
                     $(document.getElementById("cover").children[1].children[1]).html(new_table);
                 }
                 i++;
